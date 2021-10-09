b821834260f50a9a9aa04c3679cbb530c3868c8d
Trey C <treyton.cowell@ontariotechu.net>
2021-10-08 21:45:18 -0400

    Finished Game
    Added Coins, a win sequence, and other OBJS

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -11,33 +11,37 @@ public class PlayerMovement : MonoBehaviour
     public float jumpSpeed = 8.0F;
     public float gravity = 15.0F;
     private Vector3 movementDirection = Vector3.zero;
+    private int coinAmount = 0;
 
-    void Start()
+    private void OnTriggerEnter(Collider collider)
     {
+        Destroy(collider.gameObject);
+        coinAmount = coinAmount + 1;
     }
+
+    //private void OnCollsionExit(Collision collision)
+    //{
+    //}
 
     void Update()
     {
         CharacterController controller = GetComponent<CharacterController>();
 
-        //if (controller.isGrounded)
-        //{
-            movementDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-            movementDirection = transform.TransformDirection(movementDirection);
+        movementDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
 
-            movementDirection *= moveSpeed;
+        movementDirection = transform.TransformDirection(movementDirection);
 
-            //if (Input.GetButton("Jump"))
-                //movementDirection.y = jumpSpeed;
-        //}
-
-        //if (Input.GetKeyDown(KeyCode.Backspace))
-        //{
-            //SceneManager.LoadScene("Assign1Scene");
-        //}
+        movementDirection *= moveSpeed;
 
         movementDirection.y -= gravity * Time.deltaTime;
 
         controller.Move(movementDirection * Time.deltaTime);
+
+        Debug.Log(coinAmount);
+
+        if (coinAmount == 10)
+        {
+            SceneManager.LoadScene("End");
+        }
     }
 }
